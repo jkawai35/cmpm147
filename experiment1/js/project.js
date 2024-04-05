2b8eb4798f9039dc73bf3f42720a1eb4501419cd
jkawai35 <jkawai@ucsc.edu>
2024-04-04 19:37:21 -0700

    Implementation of generator for song titles

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,54 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    protag: ["Lover's", "His", "Her", "Their"],
+    relationship: ["relationship", "situationship", "friendship"],
+    ending: ["good", "bad"],
+    adjective: ["caring", "understanding", "generous", "patient", "sweet", "thoughtful", "kind"],
+    noun: ["Cry", "Song", "Heartbreak", "Happiness", "Joy", "Sadness"],
+    message: ["call", "txt"],
+    certainty: ["sure", "unsure"],
+    
+  };
+  
+  const template = `Song title: $protag $noun
+  Your $relationship with that special someone in your life turned into something $ending.
+  That special person in your life was/is $adjective.
+  You wish that you could $message them all the time about your life and everything good that happens to you.
+  You are $certainty that this will work out.
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
+  
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
